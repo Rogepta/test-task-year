871ef78f5ce782e293a2f4f52c58c2a89f962fe3
Daniil Dergunov <100157798+Rogepta@users.noreply.github.com>
2023-11-23 00:25:47 +0300

    3 commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+   colors: {
+      primaryClr: "#3877EE",
+      secondaryClr: "#EF5DA8",
+      bgc: "#FFFFFF",
+      gradient: 'linear-gradient(180deg, #3877EE -5%, #EF5DA8 85%)'
+
+   }
+}
